73780145958948f277fedd3493940c3c4cf22382
Robb Marsee <robb.marsee@gamil.com>
2017-04-19 09:31:50 -0700

    update for portfolio

--- client/app/app.js
+++ client/app/app.js
@@ -3,121 +3,26 @@ angular.module('fantasyDragRace', [
   'ui.bootstrap',
 ])
   .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/portfolio');
     $stateProvider
-    .state('mobile', {
-      url: '/mobile',
+    .state('portfolio', {
+      url: '/portfolio',
       views: {
         '': { templateUrl: './app/views/index.html',
         },
-        'landing@mobile': {
-          templateUrl: './app/partials/mobile.html',
-        },
-        'about@mobile': {
+        'about@portfolio': {
           templateUrl: './app/partials/about.html',
         },
-        'rules@mobile': {
-          templateUrl: './app/partials/rules.html',
-        },
-        'selection@mobile': {
-          templateUrl: './app/partials/signedOut/selection.html',
-          controller: 'SelectionController',
-        },
-        'rankings@mobile': {
-          templateUrl: './app/partials/rankings.html',
-          controller: 'RankingController',
-        },
-        'aboutme@mobile': {
-          templateUrl: './app/partials/aboutme.html',
-        },
-      },
-    })
-    // .state('processing', {
-    //   url: '/processing',
-    //   data: {
-    //     requireLogin: false,
-    //   },
-    //   views: {
-    //     '': { templateUrl: './app/views/index.html',
-    //     },
-    //     'landing@processing': {
-    //       templateUrl: './app/partials/processing.html',
-    //     },
-    //   },
-    // })
-    .state('login', {
-      url: '/login',
-      views: {
-        '': { templateUrl: './app/views/index.html',
-        },
-        'landing@login': {
-          templateUrl: './app/partials/signedOut/landing.html',
-          controller: 'AuthController',
-        },
-        'about@login': {
-          templateUrl: './app/partials/about.html',
-        },
-        'rules@login': {
-          templateUrl: './app/partials/rules.html',
-        },
-        'selection@login': {
-          templateUrl: './app/partials/signedOut/selection.html',
-          controller: 'SelectionController',
-        },
-        'rankings@login': {
-          templateUrl: './app/partials/rankings.html',
-          controller: 'RankingController',
-        },
-        'aboutme@login': {
-          templateUrl: './app/partials/aboutme.html',
-        },
-      },
-    })
-    .state('private', {
-      url: '/home',
 
-      views: {
-        '': { templateUrl: './app/views/index.html',
-        },
-        'landing@private': {
-          templateUrl: './app/partials/signedIn/landing.html',
-          controller: 'AuthController',
-        },
-        'navbar@private': {
-          templateUrl: './app/partials/signedIn/navbar.html',
-        },
-        'about@private': {
-          templateUrl: './app/partials/about.html',
-        },
-        'rules@private': {
-          templateUrl: './app/partials/rules.html',
-        },
-        'selection@private': {
-          templateUrl: './app/partials/signedIn/selection.html',
+        'selection@portfolio': {
+          templateUrl: './app/partials/signedOut/selection.html',
           controller: 'SelectionController',
         },
-        'rankings@private': {
-          templateUrl: './app/partials/rankings.html',
-          controller: 'RankingController',
-        },
-        'aboutme@private': {
+
+        'aboutme@portfolio': {
           templateUrl: './app/partials/aboutme.html',
         },
       },
     })
-    .state('results', {
-      url: '/results',
-      data: {
-        requireLogin: false,
-      },
-      views: {
-        '': { templateUrl: './app/views/index.html',
-      },
-        'resultForm@results': {
-          templateUrl: './app/partials/resultForm.html',
-          controller: 'ResultsController',
-        },
-      },
-    });
     $locationProvider.html5Mode(true);
   });
--- client/app/controllers/selectionController.js
+++ client/app/controllers/selectionController.js
@@ -1,202 +1,46 @@
 angular.module('fantasyDragRace')
 .controller('SelectionController', function($scope, $window, $http, $rootScope, $modal) {
-  $scope.window = window;
-  $scope.submitModal = function(type) {
-    $modal.open({
-      templateUrl: `./app/partials/modals/${type}.html`,
-      controller: 'InvalidLogin',
-    });
-  };
-
-  $scope.submitTopThree = function(selectionObj) {
-    if ($rootScope.user === undefined) {
-      $scope.submitModal('errorSubmit')
-    } else {
-      const selection = {
-        user: $rootScope.user,
-        winnerTopThreeID: selectionObj.winner,
-        runnerUpTopThreeID: selectionObj.runnerUp,
-        topThreeID: selectionObj.topThree,
-        conID: selectionObj.con,
-      };
-      if (selectionObj.winner === null ||
-          selectionObj.runnerUp === null ||
-          selectionObj.topThree === null ||
-          selectionObj.con === null) {
-        $scope.submitModal('invalidSubmit');
-      } else {
-        return $http({
-          method: 'POST',
-          url: 'api/finalSelection',
-          type: 'application/json',
-          data: selection,
-        })
-        .then((res) => {
-          $scope.submitModal('submitTop');
-          $window.location.href = '#portfolio';
-        });
-      }
-    }
-  };
-
-  $scope.submitWeeklySelection = function(selectionObj) {
-    if ($rootScope.user === undefined) {
-      $scope.submitModal('errorSubmit')
-    } else {
-      const selection = {
-        user: $rootScope.user,
-        winnerID: selectionObj.winner,
-        runnerUpID: selectionObj.runnerUp,
-        bottomID: selectionObj.bottom,
-        eliminatedID: selectionObj.eliminated,
-      };
-      if (selectionObj.winner === null ||
-          selectionObj.wrunnerUp === null ||
-          selectionObj.bottom === null ||
-          selectionObj.eliminated === null) {
-        $scope.submitModal('invalidSubmit');
-      } else {
-        return $http({
-          method: 'POST',
-          url: 'api/weeklySelection',
-          type: 'application/json',
-          data: selection,
-        })
-        .then((res) => {
-          $scope.submitModal('submitWeekly');
-          $window.location.href = '#portfolio';
-        });
-      }
-      
-    }
-  };
-
-  $scope.submitTootBootSelection = function() {
-    if ($rootScope.user === undefined) {
-      $scope.submitModal('errorSubmit');
-    } else {
-      const selection = {
-        user: $rootScope.user,
-        selection: $scope.tootBoot,
-      };
-      return $http({
-        method: 'POST',
-        url: 'api/tootBootSelection',
-        type: 'application/json',
-        data: selection,
-      })
-      .then((res) => {
-        $scope.submitModal('submitToot');
-        $window.location.href = '#portfolio';
-      });
-    }
-  };
-
-  $scope.createTeam = function(type) {
-    const modalInstance = $modal.open({
-      templateUrl: `./app/partials/modals/${type}.html`,
-      controller: 'ModalInstanceController',
-    });
-  };
-
-  $scope.joinTeam = function(type) {
-    const modalInstance = $modal.open({
-      templateUrl: `./app/partials/modals/${type}.html`,
-      controller: 'ModalInstanceController',
-    });
-  };
-
-  $scope.tootBoot = [true, true, true, true, true, true, true, true, true, true, true, true, true];
-  $scope.toggleTootBoot = function(index) {
-    $scope.tootBoot[index] = !$scope.tootBoot[index];
-  };
-
-  $scope.weeklyPick = ['Winner', 'RunnerUp', 'Lip-sync', 'Eliminated'];
-
   $scope.queens = [
-    { queenID: 1,
-      name: "Aja",
-      entrance: "Your edges are officially snatched",
-      age: 22,
-      hometown: "Brooklyn",
+    { appID: 1,
+      name: "neARby",
+      photo: "neARby.jpg",
+      photo2: "neARby2.jpg",
+      description: "An augmented Reality iOS application which allows users to see nearby events and places",
+      stack: "Tech Stack: React Native, Redux, three.js, Node.js, Express ",
+      point1: "-  Constructed the entire modular backend that successfully routes request from the frontend, based on user’s location",
+      point2: "−  Used Google Maps/Directions, Eventful and Flickr APIs to create a simplistic UI, aggregating relevant data",
+      point3: "− Implemented an algorithm which calculated the position and distance so POIs were rendered correctly in AR view",
+      point4: "−  Designed a database that expired or persisted user created places/events based on user input"
     },
-    { queenID: 2,
-      name: "Alexis Michelle",
-      entrance: "How do you like them egg rolls, Mr. Charles?",
-      age: 33,
-      hometown: "New York City",
+    { appID: 2,
+      name: "ReelTime",
+      photo: "ReelTime.jpg",
+      photo2: "ReelTime2.jpg",
+      description: "Peer to peer video streaming service using WebRTC with video and text chats",
+      stack: "Tech Stack: React, Node.js, Express, WebRTC, Socket.IO",
+      point1: "−  Expand features from sending video via links to sending video/gif individual users through an online portal",
+      point2: "− Implemented authentication for the application using bcrypt and express-session",
+      point3: "− Introduced gif search functionality using the Giphy API, allowing users to send gif messages"
+
     },
-    { queenID: 3,
-      name: "Charlie Hides",
-      entrance: "Hi, hi, hi! It's about to get shady up in here!",
-      age: 52,
-      hometown: "London",
+    { appID: 3,
+      name: "Vime",
+      photo: "Vime.jpg",
+      photo2: "Vime2.jpg",
+      description: "A messaging platform where you can only send videos and gifs messages to friends",
+      stack: "Tech Stack: React, React Router, Node.js, Express, PostgreSQL, WebGL" ,
+      point1: "−  Created a peer-to-peer connection using PeerJS to transmit text and video files between two client browsers",
+      point2: "− Designed a simplistic user interface easily allowing users to connect with each other and stream video by drag a file"
      },
-    { queenID: 4,
-      name: "Eureka OHara",
-      entrance: "Give it up for the big girl! Serving pure catfish",
-      age: 25,
-      hometown: "Johnson City, TN",
+    { appID: 4,
+      name: "RuPaul Fantasy Drag Race",
+      photo: "RuPaul.jpg",
+      photo2: "RuPaul2.jpg",
+      description: "An interactive game where players earn points each week based on predictions of who will be eliminated",
+      stack: "Tech Stack: AngularJS, Node.js, Express, PostgreSQL",
+      point1: "−  Designed a database schema which records user votes and updates a player’s score each week",
+      point2: "− Implemented a scoreboard, allowing users to see how they rank with their friends and other users"
     },
-    { queenID: 5,
-      name: "Farrah Moan",
-      entrance: "So this is what it looks like",
-      age: 23,
-      hometown: "Las Vegas",
-    },
-    { queenID: 6,
-      name: "Jaymes Mansfield",
-      entrance: "Hi shapeshifters!",
-      age: 26,
-      hometown: "Madison, WI",
-    },
-    { queenID: 7,
-      name: "Kimora Blac",
-      entrance: "Is it me or is it hot in here?",
-      age: 28,
-      hometown: "Las Vegas",
-    },
-    { queenID: 8,
-      name: "Nina BoNina Brown",
-      entrance: "I'm Nina, Bo'nina, Banana, Fofana, Osama, Bin Laden Brown! Boom boom boom boom",
-      age: 34,
-      hometown: "Alanta",
-    },
-    { queenID: 9,
-      name: "Peppermint",
-      entrance: "Heeey!",
-      age: 37,
-      hometown: "New York City",
-    },
-    { queenID: 10,
-      name: "Sasha Velour",
-      entrance: "AHHHHH!?",
-      age: 29,
-      hometown: "Brooklyn",
-    },
-    { queenID: 11,
-      name: "Shea Coulee",
-      entrance: "My name is Shea Couleé and I didn't come to play, I came to slay",
-      age: 27,
-      hometown: "Chicago",
-    },
-    { queenID: 12,
-      name: "Trinity Taylor",
-      entrance: "The body is here, bitches!",
-      age: 31,
-      hometown: "Orlando",
-    },
-    { queenID: 13,
-      name: "Valentina",
-      entrance: "Hello, it's me... Valentina!",
-      age: 25,
-      hometown: "Los Angeles",
-    },
-          { queenID: 14,
-        name: "Cynthia Lee Fontaine",
-        // entrance: "Hello, it's me... Valentina!",
-        age: 35,
-        hometown: "Austin",
-      },
+
   ];
 });
